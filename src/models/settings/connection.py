9d76c5e5c8a91085e5991638666f98a9a0c239ab
Leonardo Siquelli Monaco <leonardosiquelli@gmail.com>
2024-04-02 18:53:32 -0300

    conexao com banco

--- src/models/settings/connection.py
+++ src/models/settings/connection.py
@@ -1,4 +1,5 @@
 from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
 class DBConnectionHandler:
     def __init__(self) -> None:
@@ -7,9 +8,20 @@ class DBConnectionHandler:
             "storage.db"
         )
         self.__engine = None
+        self.__session = None
 
     def connect_to_db(self) -> None :
         self.__engine = create_engine(self.__connection_string)
 
     def get_engine(self):
         return self.__engine
+    
+    def __enter__(self):
+        #print('Estou Entrando')
+        sessrio_maker = sessionmaker()
+        self.__session = sessrio_maker(bind = self.__engine)
+        return self
+        
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        #print('Estou saindo')
+        self.__session.close()
